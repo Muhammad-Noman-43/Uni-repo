87c5397516cefd07c9d787e11032170a7e92f64a
Muhammad Noman <muhammadnoman.cs@gmail.com>
2024-12-11 18:01:06 +0000

    This is a test class for applying application to application inheritance (mistakenly named as PtoP inheritance). This includes creating a .JAR file after cleaning and building the application which is to be inherited to another folder/application. This JAR file, found in a DIST folder created in the 'to be inherited' folder after clean and build, is to be included or added to the libraries of the folder where it is to be inherited

---
+++ PtoPInheritanceTest/src/ptopinheritancetest/TestClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ptopinheritancetest;
+
+/**
+ *
+ * @author Muhammad Noman
+ */
+public class TestClass {
+    public void testMsg(){
+        System.out.println("This is a test message from the PtoPInheritance Package");
+    }
+}
